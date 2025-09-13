7cf7aa83dafea9cf88c4c4ff100217d4c3c51a0a
unknown <taha1509.ahmed32@gmail.com>
2025-09-13 03:56:49 +0300

    commennt_8

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,15 @@
+# Define the two variabless
+number1 = 10
+number2 = 5
+sum = number1 + number2
+Subtraction = number1 - number2
+Multiplication = number1* number2
+
+# Calculate the sum
+print("Addition of", number1, "and", number2, "is", sum)
+
+# Calculate the difference 
+print("Subtraction of", number1, "and", number2, "is", Subtraction )
+
+# Calculate the product
+print("Multiplication of", number1, "and", number2, "is", Multiplication)
