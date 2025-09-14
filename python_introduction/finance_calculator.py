e58a3c3ba5956bea79eafca47557fd625364af53
unknown <taha1509.ahmed32@gmail.com>
2025-09-14 04:40:33 +0300

    switching to float

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,8 +1,8 @@
 # defining variables
-monthly_income = input("Enter your monthly income: ")
-monthly_expenses = input("Enter your total monthly expenses: ")
-monthly_savings = int(monthly_income) - int(monthly_expenses)
-Projected_Savings = int(monthly_savings) * 12 + (int(monthly_savings) * 12 *0.05)
+monthly_income = float(input("Enter your monthly income: "))
+monthly_expenses = float(input("Enter your total monthly expenses: "))
+monthly_savings = monthly_income - monthly_expenses
+Projected_Savings = monthly_savings * 12 + monthly_savings * 12 *0.05
 
-print ( "Your monthly savings are" , monthly_savings)
-print ("Projected savings after one year, with interest, is:" , int(Projected_Savings))
+print ( "Your monthly savings are:$" , monthly_savings)
+print ("Projected savings after one year, with interest, is:$" , Projected_Savings)
