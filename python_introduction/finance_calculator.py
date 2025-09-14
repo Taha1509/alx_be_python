69656b5fd189fdeb44486c0ae7cdb24665573e60
unknown <taha1509.ahmed32@gmail.com>
2025-09-14 04:30:50 +0300

    adding int to the result

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -5,4 +5,4 @@ monthly_savings = int(monthly_income) - int(monthly_expenses)
 Projected_Savings = int(monthly_savings) * 12 + (int(monthly_savings) * 12 *0.05)
 
 print ( "Your monthly savings are" , monthly_savings)
-print ("Projected savings after one year, with interest, is:" , Projected_Savings)
+print ("Projected savings after one year, with interest, is:" , int(Projected_Savings))
