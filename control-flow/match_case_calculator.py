8880a611b73544e509affa8b1032e9502d42b2c8
unknown <taha1509.ahmed32@gmail.com>
2025-09-18 03:51:12 +0300

    match_case_calculator.py_11

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,19 @@
+first_number = int(input("Enter the first number: "))
+second_number = int(input("Enter the second number: "))
+operation = input("Choose the operation (+, -, *, /): ")
+
+match operation:
+    case "+":
+        print ("The result is" , first_number + second_number)
+    case "-":
+        print ("The result is" , first_number - second_number)
+    case "*":
+        print ("The result is" , first_number * second_number)
+    case "/":
+        if second_number == 0:
+            print ("Cannot divide by zero.")
+        else:
+            print ("The result is" , first_number / second_number)
+    case _:
+        print("Invalid operation selected!")
+    
