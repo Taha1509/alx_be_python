66fa11e1490ddfee9df425b29269d27862189f6b
unknown <taha1509.ahmed32@gmail.com>
2025-10-06 06:04:06 +0300

    library_management.py_1

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -1,33 +1,61 @@
 class Book:
-    def __init__(self, title, author): 
+    def __init__(self, title, author):
         self.title = title
         self.author = author
-        self.is_checked_out = False
+        self._is_checked_out = False  # Private attribute
+    
+    def check_out(self):
+        """Mark the book as checked out"""
+        if not self._is_checked_out:
+            self._is_checked_out = True
+            return True
+        return False
+    
+    def return_book(self):
+        """Mark the book as returned/available"""
+        if self._is_checked_out:
+            self._is_checked_out = False
+            return True
+        return False
+    
+    def is_available(self):
+        """Check if the book is available"""
+        return not self._is_checked_out
+    
+    def __str__(self):
+        """String representation of the book"""
+        return f"{self.title} by {self.author}"
+
 
 class Library:
     def __init__(self):
-        self.books = []
-
-    def add_book(self, title, author):
-        new_book = Book(title, author)
-        self.books.append(new_book)
-
-    def list_available_books(self):
-        # Fixed: Return only available books and fixed syntax error
-        return [book for book in self.books if not book.is_checked_out]
+        self._books = []  # Private list to store books
+    
+    def add_book(self, book):
+        """Add a book to the library"""
+        self._books.append(book)
     
     def check_out_book(self, title):
-        # Fixed: Find existing book by title, don't create new one
-        for book in self.books:
-            if book.title == title and not book.is_checked_out:
-                book.is_checked_out = True
+        """Check out a book by title"""
+        for book in self._books:
+            if book.title == title and book.is_available():
+                book.check_out()
                 return True
-        return False  # Book not found or already checked out
-
-    def return_book(self, title): 
-        # Fixed: Find existing book by title, don't create new one
-        for book in self.books:
-            if book.title == title and book.is_checked_out:
-                book.is_checked_out = False
+        return False
+    
+    def return_book(self, title):
+        """Return a book by title"""
+        for book in self._books:
+            if book.title == title and not book.is_available():
+                book.return_book()
                 return True
-        return False  # Book not found or not checked out
+        return False
+    
+    def list_available_books(self):
+        """List all available books"""
+        available_books = [book for book in self._books if book.is_available()]
+        if available_books:
+            for book in available_books:
+                print(book)
+        else:
+            print("No books available")
