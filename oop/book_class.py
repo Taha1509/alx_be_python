24c4083ccac6715eb65f4c51ae804e8a13c3a9a1
unknown <taha1509.ahmed32@gmail.com>
2025-10-11 06:33:25 +0300

    book_class.py_2

--- oop/book_class.py
+++ oop/book_class.py
@@ -11,5 +11,4 @@ class Book:
         return f"{self.title} by {self.author}, published in {self.year}"
     
     def __repr__(self):
-        return f"{self.title} by {self.author}, published in {self.year}"
-
+        return f"Book('{self.title}', '{self.author}', {self.year})"
