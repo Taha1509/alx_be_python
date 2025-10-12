25fde77f26bf05d189c93ccc6a7b7155de322ca9
unknown <taha1509.ahmed32@gmail.com>
2025-10-12 23:24:10 +0300

    polymorphism_demo.py_2

--- oop/polymorphism_demo.py
+++ oop/polymorphism_demo.py
@@ -1,17 +1,16 @@
 import math
 
-
 class Shape:
     def area(self):
         raise NotImplementedError
         
 class Rectangle(Shape):
-    def __init__ (self, lengh, width):
-        self.lengh = lengh
+    def __init__(self, length, width):  # Fixed parameter name
+        self.length = length  # Fixed: 'length' not 'lengh'
         self.width = width
 
     def area(self):
-        return self.lengh * self.width
+        return self.length * self.width  # Fixed: 'length' not 'lengh'
     
 class Circle(Shape):
     def __init__(self, radius):
